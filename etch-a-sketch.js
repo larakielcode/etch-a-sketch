c75d6589b677a81c444b31f88f4cf98d57fa5abb
ezekielmyson <deepcut.atm@gmail.com>
2025-04-17 20:09:03 +0800

    Added a function to change the background color of all the target div cells upon mouse hovering. Modified the css to have a fixed width of the container Added a header and h1 to the index.html

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,6 +5,14 @@ const container = document.querySelector(".container");
 for (let i = 1; i <= 16; i++) {
    for (let j = 1; j <= 16; j++) {
       const div = document.createElement("div");
+      div.setAttribute("class", "gridbox");
       container.appendChild(div);
    }
+}
+const gridCells = document.querySelectorAll(".gridbox");
+gridCells.forEach(cell => cell.addEventListener("mouseenter", changebgcolor));
+
+
+function changebgcolor() {
+   this.style.backgroundColor = "#5C40BC";
 }
