53383dba6e4dcb3de94197a9649de161a2e3cf5d
ezekielmyson <deepcut.atm@gmail.com>
2025-04-17 19:34:19 +0800

    Added a css file to the project.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,10 @@
 /* Author: Aldin Moreno */
+
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 16; i++) {
+   for (let j = 1; j <= 16; j++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
+   }
+}
